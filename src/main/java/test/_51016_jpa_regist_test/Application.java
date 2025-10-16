a27699fe64fd625caeadd35c679db3ed519fceaf
DESKTOP-GANN51F\0 <rodtn0gmail.com>
2025-10-16 23:26:58 +0900

    251016 JPA 등록기능 테스트

---
+++ src/main/java/test/_51016_jpa_regist_test/Application.java
@@ -0,0 +1,13 @@
+package test._51016_jpa_regist_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
